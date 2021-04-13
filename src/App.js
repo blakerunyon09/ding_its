fd4367d113393e4d9cab66937c7e3bc7b4d28b17
Jblack017 <blackhamjoshua@comcast.net>
2021-04-13 11:21:39 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
-import './App.css';
-import { Component } from 'react'
+import "./App.css";
+import { Component } from "react";
 
 class App extends Component {
-
   state = {
-    recipes: []
-  }
+    recipes: [],
+  };
 
-  APP_ID = '61c15b49'
-  APP_KEY = '0bb89a89fa57510d24eabaf14ef99a78'
+  APP_ID = "61c15b49";
+  APP_KEY = "0bb89a89fa57510d24eabaf14ef99a78";
 
   getRecipes = () => {
-    fetch(`https://api.edamam.com/search?q=chicken&app_id=${this.APP_ID}&app_key=${this.APP_KEY}`)
-    .then(res => res.json())
-    .then(({ hits }) => this.setState({
-      recipes: hits
-    }))
+    fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${this.APP_ID}&app_key=${this.APP_KEY}`
+    )
+      .then(res => res.json())
+      .then(console.log);
+  };
+
+  componentDidMount() {
+    this.getRecipes();
   }
 
-  componentDidMount(){
-    this.getRecipes()
+  render() {
+    return <div className='App'></div>;
   }
-
-  render(){
-    return (
-      <div className="App">
-  
-      </div>
-    );
-  }
-
 }
 
 export default App;
